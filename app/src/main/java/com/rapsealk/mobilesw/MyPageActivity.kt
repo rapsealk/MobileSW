432ca7566c81e60dc0ea26c95e799c43c9cc3426
rapsealk <piono623@naver.com>
2017-10-05 00:16:23 +0900

    Save Photos on Device

--- app/src/main/java/com/rapsealk/mobilesw/MyPageActivity.kt
+++ app/src/main/java/com/rapsealk/mobilesw/MyPageActivity.kt
@@ -1,9 +1,15 @@
 package com.rapsealk.mobilesw
 
+import android.Manifest
 import android.app.ProgressDialog
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -16,8 +22,13 @@ import com.rapsealk.mobilesw.schema.Photo
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Transformation
 import kotlinx.android.synthetic.main.activity_my_page.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MyPageActivity : AppCompatActivity() {
+
+    private val WRITE_EXTERNAL_STORAGE_CODE: Int = 10001
 
     private var mFirebaseAuth: FirebaseAuth? = null
     private var mFirebaseDatabase: FirebaseDatabase? = null
@@ -25,6 +36,13 @@ class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_page)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                toast("이미지를 저장하기 위해서는 권한이 필요합니다.")
+            }
+            ActivityCompat.requestPermissions(this, Array<String>(1) { Manifest.permission.WRITE_EXTERNAL_STORAGE }, WRITE_EXTERNAL_STORAGE_CODE)
+        }
 
         var progressDialog = ProgressDialog(this)
         progressDialog.isIndeterminate = true
@@ -65,7 +83,44 @@ class MyPageActivity : AppCompatActivity() {
                                             var ratio = source.width / source.height
                                             var targetHeight = (targetWidth * ratio).toInt()
                                             var result = Bitmap.createScaledBitmap(source, targetWidth, targetHeight, false)
+
+                                            // TODO : DOWNLOAD
+                                            imageView.setOnClickListener { v: View? ->
+                                                var thread = object : Runnable {
+                                                    override fun run() {
+                                                        var mime = photo.url.split(".").last().split("?").first()
+                                                        var timestamp = System.currentTimeMillis()
+                                                        var filename = "$timestamp.$mime"
+
+                                                        var directory = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "PhotoPlace")
+                                                        if (!directory.exists()) directory.mkdir()
+
+                                                        var file = File(directory.path + File.separator + filename)
+                                                        try {
+                                                            file.createNewFile()
+                                                            var ostream = FileOutputStream(file)
+                                                            when (mime.toLowerCase()) {
+                                                                "jpg", "jpeg" -> {
+                                                                    result.compress(Bitmap.CompressFormat.JPEG, 100, ostream)
+                                                                }
+                                                                "png" -> {
+                                                                    result.compress(Bitmap.CompressFormat.PNG, 100, ostream)
+                                                                }
+                                                            }
+                                                            ostream.flush()
+                                                            ostream.close()
+                                                            toast("사진이 저장됐습니다.")
+                                                        }
+                                                        catch (exception: IOException) {
+                                                            toast(exception.toString())
+                                                        }
+                                                    }
+                                                }
+                                                thread.run()
+                                            }
+
                                             if (result != source) source.recycle()
+
                                             return result
                                         }
                                     })
@@ -76,10 +131,6 @@ class MyPageActivity : AppCompatActivity() {
                                 horizontalLayout = LinearLayout(this@MyPageActivity)
                                 horizontalLayout.layoutMode = LinearLayout.HORIZONTAL
                             }
-
-                            imageView.setOnClickListener { v: View? ->
-                                toast("url: " + photo.url)
-                            }
                         }
                         progressDialog.dismiss()
                     }
@@ -89,5 +140,19 @@ class MyPageActivity : AppCompatActivity() {
                         // TODO
                     }
                 })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            WRITE_EXTERNAL_STORAGE_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    toast("ACCESS_WRITE_EXTERNAL_STORAGE PERMISSION GRANTED")
+                else finish()
+                return
+            }
+            else -> {
+                // finish()
+            }
+        }
     }
 }
